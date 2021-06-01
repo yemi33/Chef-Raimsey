7c5cd9deca9e6fc4ebac016cde4e2d3bdfa6dd6f
sue-he <59747665+sue-he@users.noreply.github.com>
2021-06-01 16:28:15 +0800

    empty input and endings
    deal with empty input and add ending word

--- InferKit/InferMain.py
+++ InferKit/InferMain.py
@@ -1,6 +1,7 @@
 import sys, os
-sys.path.append('/Users/yemishin/Desktop/Final-Project-carlcs322s01s21-2/InferKit') # for Yemi's local
+#sys.path.append('/Users/yemishin/Desktop/Final-Project-carlcs322s01s21-2/InferKit') # for Yemi's local
 #sys.path.append('/Users/maanyagoenka/Final-Project-carlcs322s01s21-2/InferKit') # for Maanya's local
+sys.path.append('/Users/xueqi/Documents/GitHub/Final-Project-carlcs322s01s21-2/InferKit') # for Sue's local
 #sys.path.append('/home/runner/Final-Project-carlcs322s01s21-2/InferKit') # for repl
 #print(sys.path)
 
@@ -42,4 +43,3 @@ class InferKit:
 			return reply['data']['text']
 
 		return None
-
--- chef_raimsey_controller.py
+++ chef_raimsey_controller.py
@@ -1,6 +1,7 @@
 from chef_raimsey import *
 from chef_raimsey_graphic import *
 from graphics import *
+import time
 
 class Chef_Raimsey_Controller:
   def start(self):
@@ -17,7 +18,7 @@ class Chef_Raimsey_Controller:
       button_yes.setFill('green')
       button_no = Rectangle(Point(17,3),Point(22,5))
       button_no_text = Text(button_no.getCenter(),"Exit")
-    
+
       button_yes.draw(window)
       button_yes_text.draw(window)
       button_no.draw(window)
@@ -29,12 +30,23 @@ class Chef_Raimsey_Controller:
         clickPoint = window.getMouse()
         if inside(clickPoint,button_yes):
           window.close()
-          self.start()  
+          self.start()
           break
         elif inside(clickPoint,button_no):
+          button_yes.undraw()
+          button_yes_text.undraw()
+          button_no.undraw()
+          button_no_text.undraw()
+          bye_box = Rectangle(Point(8,3),Point(22,5))
+          bye_text = Text(bye_box.getCenter(), "I hope you enjoying the recipe. Bye!")
+          bye_box.draw(window)
+          bye_text.draw(window)
+          time.sleep(10)
+          bye_box.undraw()
+          button_no_text.undraw()
           window.close()
           break
-      
+
       button_yes.undraw()
       button_yes_text.undraw()
       button_no.undraw()
@@ -54,4 +66,3 @@ def inside(point, rectangle):
 if __name__ == "__main__":
   controller = Chef_Raimsey_Controller()
   controller.start()
-  
